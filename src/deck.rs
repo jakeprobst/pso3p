ba461795d6cc7afe2e7e2ce43512b8879e02a6fa
Jake Probst <jake.probst@gmail.com>
2017-10-24 22:45:00 -0700

    deckbuilder, deck functions

--- src/deck.rs
+++ src/deck.rs
@@ -3,15 +3,75 @@
 
 
 use card::{Card, CharacterCard};
+use std;
+use std::result::Result;
+use std::option::Option;
+use rand::Rng;
 
 
 
+pub enum DeckType {
+    Hunter,
+    Arkz,
+}
 
+
+#[derive(Debug)]
+pub enum DeckError {
+    InvalidCardCount,
+    NoStoryCharacter,
+    InvalidStoryCharacter(CharacterCard),
+    InvalidCard(Card),
+}
+
+pub struct DeckBuilder {
+    dtype: Option<DeckType>,
+    story_character: Option<Card>,
+    cards: Vec<Card>,
+}
+
+
+impl DeckBuilder {
+    pub fn new() -> DeckBuilder {
+        DeckBuilder {
+            dtype: None,
+            story_character: None,
+            cards: Vec::new(),
+        }
+    }
+
+    pub fn faction(mut self, dtype: DeckType) -> DeckBuilder {
+        self.dtype = Some(dtype);
+        self
+    }
+    
+    pub fn character(mut self, sc: Card) -> DeckBuilder {
+        self.story_character = Some(sc);
+        self
+    }
+    
+    pub fn card(mut self, card: Card) -> DeckBuilder {
+        self.cards.push(card);
+        self
+    }
+
+    fn is_valid(&self) -> bool {
+        true
+    }
+    
+    pub fn deck(self) -> std::result::Result<Deck, DeckError> {
+        Ok(Deck {
+            dtype: self.dtype.unwrap(),
+            story_character: self.story_character.unwrap(),
+            cards: self.cards,
+        })
+    }
+}
 
 pub struct Deck {
-    //character: CharacterCard,
+    story_character: Card,
+    dtype: DeckType,
     cards: Vec<Card>,
-    
 }
 
 
@@ -19,9 +79,15 @@ pub struct Deck {
 
 
 impl Deck {
-    pub fn new() -> Deck {
-        Deck {
-            cards: Vec::new(),
-        }
+    pub fn shuffle<R: Rng>(&mut self, rng: &mut R)  {
+        rng.shuffle(self.cards);
+    }
+
+    pub fn draw(&mut self) -> Card {
+        self.cards.pop()
+    }
+
+    pub fn discard(&mut self, card: Card) {
+        self.cards.push(card);
     }
 }
